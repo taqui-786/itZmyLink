e0e641e5b286dfe5377086d475898ca3b22ad8fe
taqui-786 <mdtaqui.jhar@gmail.com>
2024-08-25 00:31:34 +0530

    Fixing Bugs and adding new feat

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
+import { updateSession } from "@/lib/supabase/supabaseMiddleware";
 import { type NextRequest } from "next/server";
-import { updateSession } from "@/lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
 	return await updateSession(request);
